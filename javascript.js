855224e7869c8f2133c3caafa8b1cb57eb5143ed
falko <115454465+FalkoKa@users.noreply.github.com>
2023-01-24 17:45:33 +0700

    Add function displayBook and clickEvents

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// APP
 let myLibrary = [];
 
 function Book(title, author, pages, status) {
@@ -10,7 +11,49 @@ function Book(title, author, pages, status) {
   };
 }
 
-// const book1 = new Book("Herr der Ringe", "Tolkin", 1212, "read");
-// const book2 = new Book("Harry Potter", "JKR", 685, "unread");
+function addBookToLibrary() {
+  let userInput = prompt("Please enter: Title, Author, Pages, Status");
+  myLibrary.push(new Book(userInput));
+  displayBook();
+}
 
-function 
+function displayBook() {
+  myLibrary.forEach((element) => {
+    const div = document.createElement("div");
+    const bookTitle = document.createElement("p");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookStatus = document.createElement("p");
+    // const removeBtn = document.createElement("button");
+
+    // removeBtn.setAttribute("id", "remove-btn");
+    // removeBtn.textContent = "Remove";
+
+    container.append(div);
+
+    bookTitle.textContent = element.title;
+    bookAuthor.textContent = element.author;
+    bookPages.textContent = element.pages;
+    bookStatus.textContent = element.status;
+
+    div.classList.add("book-card");
+    div.append(bookTitle);
+    div.append(bookAuthor);
+    div.append(bookPages);
+    div.append(bookStatus);
+    const bookCard = document.querySelector(".book-card");
+    // bookCard.append(removeBtn);
+
+    // removeBtn.addEventListener("click", () => div.remove(".book-card"));
+  });
+}
+
+// UI
+const container = document.querySelector(".container");
+const btn = document.querySelector(".addBook");
+
+btn.addEventListener("click", handleClick);
+
+function handleClick() {
+  addBookToLibrary();
+}
